28dffdb26bed05aae5fe67d57d70e596776a2854
Brendan Mulcahy <bmm58@case.edu>
2014-10-12 16:22:10 -0700

    Started making the audio mixer

---
+++ Assets/Scripts/AudioMixer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AudioMixer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float mix(List<float> audioSamples)
+	{
+		float output = 0f;
+
+		//TODO: Make this math magical
+		foreach (float sample in audioSamples)
+		{
+			output += sample;
+		}
+	}
+}
