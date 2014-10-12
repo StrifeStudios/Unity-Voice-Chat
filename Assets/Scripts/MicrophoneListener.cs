8b2bc65b351942bc634fef4596c0334d174e4307
Raverenx <raverenx@gmail.com>
2014-10-11 18:52:33 -0700

    Recorder with playback

--- Assets/Scripts/MicrophoneListener.cs
+++ Assets/Scripts/MicrophoneListener.cs
@@ -3,5 +3,44 @@ using System.Collections;
 
 public class MicrophoneListener : MonoBehaviour
 {
+    private string selectedDevice = null;
+    [SerializeField]
+    private int recordingFrequency = 10000;
+    private AudioClip audioClip;
+    void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        foreach (var device in Microphone.devices)
+        {
+            if (GUILayout.Button(device))
+            {
+                this.selectedDevice = device;
+            }
+        }
 
+        GUILayout.Space(50);
+        if (selectedDevice != null)
+        {
+            if (GUILayout.Button("Record"))
+            {
+                RecordButtonPressed();
+            }
+            if (GUILayout.Button("Stop"))
+            {
+                StopButtonPressed();
+            }
+        }
+        GUILayout.EndVertical();
+    }
+
+    private void RecordButtonPressed()
+    {
+        this.audioClip = Microphone.Start(selectedDevice, false, 10, recordingFrequency);
+    }
+
+    private void StopButtonPressed()
+    {
+        Microphone.End(selectedDevice);
+        this.audio.PlayOneShot(audioClip);
+    }
 }
