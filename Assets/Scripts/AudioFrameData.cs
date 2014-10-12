f73765014a1c8057fe3d6563ccdb2057532edfca
Raverenx <raverenx@gmail.com>
2014-10-12 04:54:42 -0700

    Cleaned up file and project structure
    Deleted unused files
    Moved AudioFrameData into its own file

---
+++ Assets/Scripts/AudioFrameData.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Immutable structure representing audio data for a single frame.
+/// </summary>
+public struct AudioFrameData
+{
+    public readonly float[] AudioData;
+    public readonly int NumChannels;
+    public AudioFrameData(float[] data, int numChannels)
+    {
+        this.AudioData = data;
+        this.NumChannels = numChannels;
+    }
+}
--- Assets/Scripts/IAudioDataProvider.cs
+++ Assets/Scripts/IAudioDataProvider.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-/// <summary>
+﻿/// <summary>
 /// Represents a type that provides audio frame data.
 /// </summary>
 public interface IAudioDataProvider
@@ -11,17 +7,3 @@ public interface IAudioDataProvider
 }
 
 public delegate void AudioDataReceivedEventHandler(AudioFrameData frameData);
-
-/// <summary>
-/// Immutable structure representing audio data for a single frame.
-/// </summary>
-public struct AudioFrameData
-{
-    public readonly float[] AudioData;
-    public readonly int NumChannels;
-    public AudioFrameData(float[] data, int numChannels)
-    {
-        this.AudioData = data;
-        this.NumChannels = numChannels;
-    }
-}
