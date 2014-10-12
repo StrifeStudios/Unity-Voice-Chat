5041fa02af8fe630f4037f5af4d7632d44979bbb
Raverenx <raverenx@gmail.com>
2014-10-11 19:01:04 -0700

    Some more options for the recorder

--- Assets/Scripts/MicrophoneListener.cs
+++ Assets/Scripts/MicrophoneListener.cs
@@ -7,15 +7,33 @@ public class MicrophoneListener : MonoBehaviour
     [SerializeField]
     private int recordingFrequency = 10000;
     private AudioClip audioClip;
+
+    private string frequencyInputString = "10000";
     void OnGUI()
     {
         GUILayout.BeginVertical();
         foreach (var device in Microphone.devices)
         {
+            if (this.selectedDevice == device)
+            {
+                GUI.contentColor = Color.cyan;
+            }
+            else
+            {
+                GUI.contentColor = Color.white;
+            }
             if (GUILayout.Button(device))
             {
                 this.selectedDevice = device;
             }
+        }
+        GUI.contentColor = Color.white;
+
+        GUILayout.Label("Recording frequency:");
+        string newFreqString = GUILayout.TextField(frequencyInputString);
+        if (int.TryParse(newFreqString, out recordingFrequency))
+        {
+            this.frequencyInputString = newFreqString;
         }
 
         GUILayout.Space(50);
